51c96c3a63ed9396c663c63305c24e071e4c8cd9
Piergiorgio Vagnozzi <piergiorgio_vagnozzi@hotmail.com>
2024-08-16 18:20:32 +0200

    FIX

--- src/shared/Sheeva.Data.Abstractions/DataSeeder.cs
+++ src/shared/Sheeva.Data.Abstractions/DataSeeder.cs
@@ -1,5 +1,6 @@
 namespace Sheeva.Data.Abstractions;
 
+using Log;
 using Microsoft.Extensions.Logging;
 using Services;
 
@@ -7,17 +8,21 @@ public abstract class DataSeeder(IUnitOfWork unitOfWork, ILogger logger) : Servi
 {
     private IUnitOfWork UnitOfWork { get; } = unitOfWork;
 
+    private static readonly LoggerAction<string> LogSeedData = new(new EventId(1, "SeedData"), "LogSeedData: {Message}");
+
+    private static readonly LoggerAction<string, string> LogSeedEntity = new(new EventId(2, "SeedEntity"), "SeedEntity entity {Entity}: {Message}");
+
     public void SeedData()
     {
         try
         {
-            this.Logger.LogInformation("SeedData");
+            this.Logger.LogTrace(LogSeedData, "started");
             this.SeedEntities();
-            this.Logger.LogInformation("SeedData: completed");
+            this.Logger.LogTrace(LogSeedData, "completed");
         }
         catch (Exception ex)
         {
-            this.Logger.LogError(ex, "SeedData: {Exception}", ex);
+            this.Logger.LogError(LogSeedData, "error", ex);
         }
     }
 
@@ -29,29 +34,29 @@ public abstract class DataSeeder(IUnitOfWork unitOfWork, ILogger logger) : Servi
     {
         try
         {
-            this.Logger.LogTrace("SeedEntity {TEntity}", typeof(TEntity).Name);
+            this.Logger.LogTrace(LogSeedEntity, typeof(TEntity).Name, "stared");
             using var repository = this.UnitOfWork.GetRepository<TKey, TEntity>();
             foreach (var entity in entities)
             {
                 try
                 {
-                    this.Logger.LogTrace("SeedEntity entity {entity}", entity);
+                    this.Logger.LogTrace(LogSeedEntity, typeof(TEntity).Name, entity.ToString() ?? string.Empty);
                     repository.Add(entity);
-                    this.Logger.LogTrace("SeedEntity entity {entity}: inserted", entity);
+                    this.Logger.LogTrace(LogSeedEntity, typeof(TEntity).Name, entity + " - completed");
 
                 }
                 catch (Exception ex)
                 {
-                    this.Logger.LogError(ex, "SeedEntity entity {entity}: {Exception}", entity, ex);
+                    this.Logger.LogError(LogSeedEntity, typeof(TEntity).Name, entity + " - " + ex.Message, ex);
                 }
             }
 
             this.UnitOfWork.SaveChanges();
-            this.Logger.LogTrace("SeedEntity {TEntity}: completed", typeof(TEntity).Name);
+            this.Logger.LogTrace(LogSeedEntity, typeof(TEntity).Name, "completed");
         }
         catch (Exception ex)
         {
-            this.Logger.LogError(ex, "SeedEntity {TEntity}: {Exception}", ex);
+            this.Logger.LogError(LogSeedEntity, typeof(TEntity).Name, ex.Message, ex);
         }
     }
 
@@ -61,7 +66,7 @@ public abstract class DataSeeder(IUnitOfWork unitOfWork, ILogger logger) : Servi
     {
         try
         {
-            this.Logger.LogTrace("SeedEntity {TEntity}", typeof(TEntity).Name);
+            this.Logger.LogTrace(LogSeedEntity, typeof(TEntity).Name, "started");
             using var repository = this.UnitOfWork.GetRepository<TKey, TEntity>();
             var currentItems = repository.List();
 
@@ -69,29 +74,29 @@ public abstract class DataSeeder(IUnitOfWork unitOfWork, ILogger logger) : Servi
             {
                 try
                 {
-                    this.Logger.LogTrace("SeedEntity entity {entity}", entity);
+                    this.Logger.LogTrace(LogSeedEntity, typeof(TEntity).Name, entity.ToString() ?? string.Empty);
                     var currentItem = currentItems.FirstOrDefault(x => finder(x, entity));
                     if (currentItem is not null)
                     {
-                        this.Logger.LogTrace("SeedEntity entity {entity}: found", entity);
+                        this.Logger.LogTrace(LogSeedEntity, typeof(TEntity).Name,  "found");
                         continue;
                     }
 
                     repository.Add(entity);
-                    this.Logger.LogTrace("SeedEntity entity {entity}: inserted", entity);
+                    this.Logger.LogTrace(LogSeedEntity, typeof(TEntity).Name, "inserted");
                 }
                 catch (Exception ex)
                 {
-                    this.Logger.LogError(ex, "SeedEntity entity {entity}: {Exception}", entity, ex);
+                    this.Logger.LogError(LogSeedEntity, typeof(TEntity).Name, ex.Message);
                 }
             }
 
             this.UnitOfWork.SaveChanges();
-            this.Logger.LogTrace("SeedEntity {TEntity}: completed", typeof(TEntity).Name);
+            this.Logger.LogTrace(LogSeedEntity, typeof(TEntity).Name, "completed");
         }
         catch (Exception ex)
         {
-            this.Logger.LogError(ex, "SeedEntity {TEntity}: {Exception}", ex);
+            this.Logger.LogError(LogSeedEntity, typeof(TEntity).Name, ex.Message);
         }
     }
 }
